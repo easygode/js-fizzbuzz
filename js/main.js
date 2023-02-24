ebfa3fd4c8a962cfa431d0b927474c6e4e244d6f
easygode <bimalbenvenuti@gmail.com>
2023-02-24 10:58:02 +0100

    esercizio corretto con bonus-2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
 'use strict';
 
+//creo una variabile container per HTML, esterno per non 
+//compromettere la performance del comando for
+const container = document.getElementById('table');
 
-//1. creo un ciclo che genera numeri da 1 a 100
+//creo un ciclo che mostra i numeri da 1 a 100
+for(let i = 1; i <= 100; i=i+1){
+
+//creo un elemento HTML div
+//creo una codizione per stampare FizzBuzz se il numero è divisibile per 3 e 5,
+// stampo Fizz se il numero è divisibile per 3,
+// stampo Buzz se il numero è divisibile per 5.
+   
+const item = document.createElement('div');
+container.append(item);
+
+   if(i % 3 === 0 && i % 5 === 0){
+   console.log('FizzBuzz');
+   item.append('FizzBuzz');
+   // opzione inner HTML item.innerHTML = 'FizzBuzz';
+   // opzione innerText item.innerText = 'FizzBuzz';
+
+   } else if(i % 3 === 0){
+      console.log('Fizz');
+      item.append('Fizz');
+      // opzione inner HTML item.innerHTML = 'Fizz';
+      // opzione innerText item.innerText = 'Fizz';
+   } else if(i % 5 === 0){
+      console.log('Buzz');
+      item.append('Buzz');
+      // opzione inner HTML item.innerHTML = 'Buzz';
+      // opzione innerText item.innerText = 'Buzz';
+   } else {
+   console.log(i);
+   item.append(i);
+   }
+}
+
+/*//1. creo un ciclo che genera ieri da 1 a 100
 
 let i = 0;
 let div = '';
@@ -10,13 +46,12 @@ let h2 = '';
 for (let i = 1; i < 101; i++) {
     console.log (i);
    }
-
    const divBox = document.createElement('div');
    divBox.classList.add(box);
 
-   const divNum = document.createElement('h2');
-   divNum.classList.add(box-num);
-   divBox.append(divNum);
+   const divi = document.createElement('h2');
+   divi.classList.add(box-i);
+   divBox.append(divi);
 
 //2. creo variabile per ul nel HTML
 const ul = document.querySelector('ul.list');
@@ -25,9 +60,9 @@ let fizzBuzz = i % 15;
 let fizz = i % 3;
 let buzz = 1 % 5;
 
-//2. SE il numero è divisibile per 3 allora stampo "Fizz" al posto del numero
-   //Altrimenti SE il numero è divisibile per 5 allora stampo "Buzz" al posto del numero
-   //Altrimenti SE il numero è divisibile per 3 e per 5 allora "stampo" FizzBuzz al posto del numero
+//2. SE il iero è divisibile per 3 allora stampo "Fizz" al posto del iero
+   //Altrimenti SE il iero è divisibile per 5 allora stampo "Buzz" al posto del iero
+   //Altrimenti SE il iero è divisibile per 3 e per 5 allora "stampo" FizzBuzz al posto del iero
 
    if (i % 15 === 0) {
     console.log("FizzBuzz");
@@ -53,7 +88,4 @@ let buzz = 1 % 5;
     let fizzBuzz = i % 15;
     console.log ('FizzBuzz');
    }
-   
-   )
-
-
+   )*/
